84f4cb7a359d4f43551cd51dfe5a880cab21c731
jwmx9 <31551248+jwmx9@users.noreply.github.com>
2019-06-16 18:55:08 -0500

    Add files via upload

---
+++ DocumentsChallenge/DocumentsChallenge/DocumentTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DocumentTableViewCell.swift
+//  DocumentsChallenge
+//
+//  Created by John Williams III on 6/14/19.
+//  Copyright © 2019 John Williams III. All rights reserved.
+//
+
+import UIKit
+
+class DocumentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
